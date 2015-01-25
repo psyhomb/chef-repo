37f423cbcc1f4530032a7aa391145635ceb49523
Milos Buncic <htchak19@gmail.com>
2015-01-25 16:16:44 +0100

    Added check for file existence

--- cookbooks/my-java/recipes/java-uniform.rb
+++ cookbooks/my-java/recipes/java-uniform.rb
@@ -13,19 +13,25 @@ java_version.each do |version, checksum|
   major = version[0...version.index("u")]
   minor = version[version.index("u")+1..-1] 
 
+  next if ::File.exists?("/usr/java/jdk1.#{major}.0_#{minor}")
+
   remote_file "/usr/java/jdk-#{version}-linux-x64.tar.gz" do
     source "http://download.oracle.com/otn-pub/java/jdk/#{version}-b13/jdk-#{version}-linux-x64.tar.gz"
     checksum "#{checksum}"
     headers({"Cookie" => "oraclelicense=accept"})
     #notifies :run, "bash[untar]", :immediately
-    not_if { ::File.exists?("/usr/java/jdk1.#{major}.0_#{minor}") }
+    #not_if { ::File.exists?("/usr/java/jdk1.#{major}.0_#{minor}") }
   end
 
   bash 'untar' do
     cwd '/usr/java'
     code <<-EOH
-      (tar xzf jdk-#{version}-linux-x64.tar.gz && ln -s jdk1.#{major}.0_#{minor} latest#{major} && rm -rf jdk-#{version}-linux-x64.tar.gz)
+    (
+      tar xzf jdk-#{version}-linux-x64.tar.gz &&\
+      ln -s jdk1.#{major}.0_#{minor} latest#{major} &&\
+      rm -rf jdk-#{version}-linux-x64.tar.gz &&\
       chown -R root. jdk1.#{major}.0_#{minor}
+    )
     EOH
     action :run
   end
