2ec37f4a192b7b4ee093fdc83870c349f6b80758
Milos Buncic <htchak19@gmail.com>
2015-01-25 22:06:27 +0100

    Added java_path var

--- cookbooks/my-java/recipes/java-uniform.rb
+++ cookbooks/my-java/recipes/java-uniform.rb
@@ -6,30 +6,31 @@
 # Checksum is SHA256
 
 java_version = node['java_version'][node.chef_environment]
+java_path = "/usr/java"
 
-directory "/usr/java"
+directory "#{java_path}"
 
 java_version.each do |version, checksum|
   major = version[0...version.index("u")]
   minor = version[version.index("u")+1..-1] 
 
-  next if ::File.exists?("/usr/java/jdk1.#{major}.0_#{minor}")
+  next if ::File.exists?("#{java_path}/jdk1.#{major}.0_#{minor}")
 
-  remote_file "/usr/java/jdk-#{version}-linux-x64.tar.gz" do
+  remote_file "#{java_path}/jdk-#{version}-linux-x64.tar.gz" do
     source "http://download.oracle.com/otn-pub/java/jdk/#{version}-b13/jdk-#{version}-linux-x64.tar.gz"
     checksum "#{checksum}"
     headers({"Cookie" => "oraclelicense=accept"})
     #notifies :run, "bash[untar]", :immediately
-    #not_if { ::File.exists?("/usr/java/jdk1.#{major}.0_#{minor}") }
+    #not_if { ::File.exists?("#{java_path}/jdk1.#{major}.0_#{minor}") }
   end
 
   bash 'untar' do
-    cwd '/usr/java'
+    cwd "#{java_path}"
     code <<-EOH
     (
       tar xzf jdk-#{version}-linux-x64.tar.gz &&\
       rm -rf jdk-#{version}-linux-x64.tar.gz &&\
-      ln -sf jdk1.#{major}.0_#{minor} latest#{major} &&\
+      ln -s jdk1.#{major}.0_#{minor} latest#{major} &&\
       chown -R root. jdk1.#{major}.0_#{minor}
     )
     EOH
@@ -39,6 +40,9 @@ end
 
 # Vast version
 #java_version = node['java_version'][node.chef_environment]
+#java_path = "/usr/java"
+#
+#directory "#{java_path}"
 #
 #java_version.each do |version, checksum|
 #  major = version[0...version.index("u")]
@@ -47,7 +51,7 @@ end
 #  java_ark "jdk-#{version}-linux-x64" do
 #    url              "http://download.oracle.com/otn-pub/java/jdk/#{version}-b13/jdk-#{version}-linux-x64.tar.gz"
 #    checksum         "#{checksum}"
-#    app_home         "/usr/java/latest#{major}"
+#    app_home         "#{java_path}/latest#{major}"
 #    action :install
 #  end
 #end
