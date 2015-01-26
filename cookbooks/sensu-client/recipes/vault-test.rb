acebe9b13000667752641c3e08f274ca589c8e23
Milos Buncic <htchak19@gmail.com>
2015-01-26 13:00:56 +0100

    Nothing major

--- cookbooks/sensu-client/recipes/vault-test.rb
+++ cookbooks/sensu-client/recipes/vault-test.rb
@@ -10,12 +10,12 @@
 chef_gem 'chef-vault'
 require 'chef-vault'
 
-vault_server = ChefVault::Item.load('certs', 'ssl')['server']
-#vault_client = ChefVault::Item.load('certs', 'ssl')['client']
+server = ChefVault::Item.load('certs', 'ssl')['server']
+client = ChefVault::Item.load('certs', 'ssl')['client']
 
 directory "/tmp/sensu"
 
-vault_server.each do |key, value|
+server.each do |key, value|
   file "/tmp/sensu/server_#{key}.pem" do
     content value
     owner 'root'
@@ -24,12 +24,11 @@ vault_server.each do |key, value|
   end
 end
 
-#%w[key cert].each do |f|
-# template "/tmp/sensu/#{f}.pem" do
-#   variables :"#{f}" => data['client'][f]
-#   source "#{f}.erb"
-#   group "root"
-#   mode "0644"
-#   action :create
-#  end
-#end
+client.each do |key, value|
+  file "/tmp/sensu/client_#{key}.pem" do
+    content value
+    owner 'root'
+    group 'root'
+    mode '644'
+  end
+end
