02bc8b50bf18c59c2ddb2211ebaadc7b12316a32
Milos Buncic <htchak19@gmail.com>
2015-02-03 15:10:32 +0100

    my-java: minor changes with logging

--- cookbooks/my-java/recipes/java3-uniform.rb
+++ cookbooks/my-java/recipes/java3-uniform.rb
@@ -1,11 +1,25 @@
-# Download jdk
-# curl -L -b "oraclelicense=a" http://download.oracle.com/otn-pub/java/jdk/8u31-b13/jdk-8u31-linux-x64.tar.gz -O
-# or
-# curl -L -H "Cookie: oraclelicense=accept" http://download.oracle.com/otn-pub/java/jdk/8u31-b13/jdk-8u31-linux-x64.tar.gz -O
+# Authors: Milos Buncic and Ivan Savcic
+# Description: Installs Oracle Java JDK based on the environment (defined through 'java-list' role)
 #
-# Checksum is SHA256
+###
+#
+# How to download Oracle Java JDK with curl
+# Example:
+#   curl -L -b "oraclelicense=a" http://download.oracle.com/otn-pub/java/jdk/8u31-b13/jdk-8u31-linux-x64.tar.gz -O
+# or
+#   curl -L -H "Cookie: oraclelicense=accept" http://download.oracle.com/otn-pub/java/jdk/8u31-b13/jdk-8u31-linux-x64.tar.gz -O
 
-# Checksum list
+# Local vars
+env = node.chef_environment
+java_home = "/usr/java"
+
+# Recipe attributes
+node.default['java_versions'][env] = nil
+
+# List of Java versions
+java_versions = node['java_versions'][env]
+
+# Hash of checksums (SHA256)
 checksums = {
   "8u31" => "efe015e8402064bce298160538aa1c18470b78603257784ec6cd07ddfa98e437",
   "7u51" => "77367c3ef36e0930bf3089fb41824f4b8cf55dcc8f43cce0868f7687a474f55c",
@@ -13,26 +27,24 @@ checksums = {
   "7u76" => "ce8ff4fed2cd16aea432edf8e94f21ccfe29e9d4a659bbbef3551982769e0c8c"
 }
 
-# Vast version
-java_versions = node['java_versions'][node.chef_environment]
-java_home = "/usr/java"
 
-abort "java_versions is nil or empty - Chef Environment: #{node.chef_environment}" if java_versions.nil? or java_versions.empty?
+# Exit if java_versions is nil
+abort "java_versions has nil value - Chef Environment: #{env}" if java_versions.nil?
 
 # If you don't wont to use abort function (previous line) this is the another way to do this
-#if java_versions.nil? or java_versions.empty?
-#  Chef::Log.error("java_versions is nil or empty - Chef Environment: #{node.chef_environment}")
-#  exit 1
+#if java_versions.nil?
+#  Chef::Log.error("java_versions is nil or empty - Chef Environment: #{env}")
 #end
 
+# Run installation of all java versions defined in java_versions list
 java_versions.each do |version|
   major = version[0...version.index("u")]
   minor = version[version.index("u")+1..-1]
 
   java_ark "jdk-#{version}-linux-x64" do
-    url              "http://java.buncici.com/jdk-#{version}-linux-x64.tar.gz"
-    checksum         "#{checksums[version]}"
-    app_home         "#{java_home}/latest#{major}"
-    action :install
+    url        "http://java.buncici.com/jdk-#{version}-linux-x64.tar.gz"
+    checksum   "#{checksums[version]}"
+    app_home   "#{java_home}/latest#{major}"
+    action     :install
   end
 end
