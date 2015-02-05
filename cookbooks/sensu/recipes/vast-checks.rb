74facaf688ce9c23cff1b431c4798fcb9341c198
Milos Buncic <htchak19@gmail.com>
2015-02-05 18:05:49 +0100

    sensu: New checks added

--- cookbooks/sensu/recipes/vast-checks.rb
+++ cookbooks/sensu/recipes/vast-checks.rb
@@ -4,5 +4,23 @@ sensu_check "check_cassandra" do
   handlers ["flapjack"]
   subscribers ["cassandra"]
   interval 30
-  additional(:notification => "Cassandra is not running!", :occurrences => 5)
+  additional(:notification => "Cassandra is not running", :occurrences => 3)
+end
+
+# Disk check
+sensu_check "check_disk" do
+  command "check_disk -w 15 -c 10 -p /"
+  handlers ["flapjack"]
+  subscribers ["base"]
+  interval 30
+  additional(:notification => "Disk space problem", :occurrences => 3)
+end
+
+# SSH check
+sensu_check "check_ssh" do
+  command "check_ssh -t 20 -p 22 :::address:::"
+  handlers ["flapjack"]
+  subscribers ["base"]
+  interval 30
+  additional(:notification => "SSH service is not running!", :occurrences => 3)
 end
