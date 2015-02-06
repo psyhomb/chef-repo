bcb22c777c96a8dea5e1bb0f0ae95f9ee58fefbd
Milos Buncic <htchak19@gmail.com>
2015-02-06 12:23:51 +0100

    vast-sensu: Changed interval for checks

--- cookbooks/vast-sensu/recipes/checks.rb
+++ cookbooks/vast-sensu/recipes/checks.rb
@@ -3,7 +3,7 @@ sensu_check "check_cassandra" do
   command "check-procs.rb -p cassandra -C 1"
   handlers ["flapjack"]
   subscribers ["cassandra"]
-  interval 30
+  interval 60
   additional(:notification => "Cassandra is not running", :occurrences => 3)
 end
 
@@ -12,7 +12,7 @@ sensu_check "check_disk" do
   command "check_disk -w 15 -c 10 -p /"
   handlers ["flapjack"]
   subscribers ["base"]
-  interval 30
+  interval 60
   additional(:notification => "Disk space problem", :occurrences => 3)
 end
 
@@ -21,6 +21,6 @@ sensu_check "check_ssh" do
   command "check_ssh -t 20 -p 22 :::address:::"
   handlers ["flapjack"]
   subscribers ["base"]
-  interval 30
+  interval 60
   additional(:notification => "SSH service is not running!", :occurrences => 3)
 end
