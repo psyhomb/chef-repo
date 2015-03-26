0d18e392e75ef206e2f3893d77a6f98301d2a5f3
Milos Buncic <htchak19@gmail.com>
2015-03-26 18:08:55 +0100

    vast-sensu-client: Added checks definitions to be executed on the server side

--- cookbooks/vast-sensu-client/recipes/checks.rb
+++ cookbooks/vast-sensu-client/recipes/checks.rb
@@ -1,3 +1,11 @@
+ruby_block "sensu_service_trigger" do
+  block do
+    # Sensu service action trigger for LWRPs.
+    # This resource must be defined before the Sensu LWRPs can be used.
+  end
+  action :nothing
+end
+
 # Cassandra process check
 sensu_check "check_cassandra" do
   type "metric"
