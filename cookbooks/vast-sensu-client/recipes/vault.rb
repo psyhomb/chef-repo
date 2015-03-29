d491a59f87a8766f08fecb05481eb54544f06d8d
Milos Buncic <htchak19@gmail.com>
2015-03-29 23:47:28 +0200

    vast-sensu-client: New comment

--- cookbooks/vast-sensu-client/recipes/vault.rb
+++ cookbooks/vast-sensu-client/recipes/vault.rb
@@ -1,7 +1,7 @@
 # Install and include new chef-vault gem
 # Note: If you want to use encrypted data bags (vault) without chef-vault cookbook then you have to 
 #       edit chef-repo/cookbooks/sensu/libraries/sensu_helpers.rb and replace the line 
-#       - chef_vault_item("sensu", item)
-#       + ChefVault::Item.load("sensu", item)
+#       - chef_vault_item(data_bag_name, item)
+#       + ChefVault::Item.load(data_bag_name, item)
 chef_gem "chef-vault"
 require "chef-vault"
